dc2f4bda6b1b923a17e680d88d021b556300c276
abhishek <abhishekudiya09@gmail.com>
2023-12-20 01:27:00 +0530

    Jwt_Middleware done

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -5,20 +5,21 @@ const express = require('express');
 const app = express();
 let requestCount = 0;
 
-// You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// maintain a count of the number of requests made to the server in the global
-// requestCount variable
+function countRequest(req, res, next) {
+  requestCount++;
+  // console.log(`Total requests: ${requestCount}`);
+  next();
+}
 
-app.get('/user', function(req, res) {
+app.get('/user',countRequest, function(req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user',countRequest, function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
-app.get('/requestCount', function(req, res) {
+app.get('/requestCount',countRequest, function(req, res) {
   res.status(200).json({ requestCount });
 });
 
--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,16 +1,31 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = "secret";
 
-function signJwt(username, password) {
-
+function signJwt(username) {
+    try {
+        const token = jwt.sign({ username: username }, jwtPassword);
+        return token;
+    } catch (error) {
+        throw new Error('Error signing JWT');
+    }
 }
 
 function verifyJwt(token) {
-
+    try {
+        const decoded = jwt.verify(token, jwtPassword);
+        return decoded;
+    } catch (error) {
+        throw new Error('Invalid JWT token');
+    }
 }
 
 function decodeJwt(token) {
-
+    try {
+        const decoded = jwt.decode(token, { complete: true });
+        return decoded;
+    } catch (error) {
+        throw new Error('Error decoding JWT');
+    }
 }
 
 module.exports = {
